f7e952e83076f03fb9edfe7de255ed5c6219bca0
bradyfowler89 <82802955+bradyfowler89@users.noreply.github.com>
2021-04-19 08:52:30 +0100

    Add files via upload

---
+++ Journey/Journey/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many kilometers is the journey? ");
+            double kilo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many passengers will there be? ");
+            int people = Convert.ToInt32(Console.ReadLine());
+            kilo = 2*(kilo - 1)+3;
+            if (people > 4)
+            {
+                kilo = kilo + (0.5 * kilo);
+            }
+            Console.WriteLine("The cost of the journey is £" + kilo);
+            Console.ReadKey();
+        }
+    }
+}
